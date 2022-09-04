6337ec74ed433e0120af8d5782816f7002aab6db
pavan432 <pavankunchala0@gmail.com>
2022-09-04 11:41:06 +0530

    java script changes

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-console.log('Testing');
+var userDetails = [
+  {
+    userName: 'pavanteja',
+    userPassword: '1234'
+  }
+];
+getUser = prompt('Enter user name');
+getPass = prompt('Enter password');
+function confirm(user, pass) {
+  if (
+    user === userDetails[0].userName &&
+    pass === userDetails[0].userPassword
+  ) {
+    console.log('Welcome!');
+  } else {
+    alert('Incorrect username or password');
+  }
+}
+confirm(getUser, getPass);
