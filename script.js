30ab0e76d0cecc9cb80abd38d212bebcfb6e0c78
pavan432 <pavankunchala0@gmail.com>
2022-09-10 15:43:12 +0530

    final change

--- script.js
+++ script.js
@@ -1,19 +1,65 @@
-var userDetails = [
-  {
-    userName: 'pavanteja',
-    userPassword: '1234'
+const form = document.getElementById('form');
+const passwordEl1 = document.getElementById('password1');
+const passwordEl2 = document.getElementById('password2');
+const messageContainer = document.querySelector('.message-container');
+const message = document.getElementById('message');
+
+let isValid = false;
+let passwordsMatch = false;
+
+function validateForm(){
+  /* --- HTML constraint API to check form validity --- */
+  isValid = form.checkValidity();
+  /* --- If form is not valid --- */
+  if(!isValid){
+    /* --- Style main message error --- */
+    message.textContent = "Please fill out all fields.";
+    message.style.color = "red";
+    messageContainer.style.borderColor = "red";
+    return;
   }
-];
-getUser = prompt('Enter user name');
-getPass = prompt('Enter password');
-function confirm(user, pass) {
-  if (
-    user === userDetails[0].userName &&
-    pass === userDetails[0].userPassword
-  ) {
-    console.log('Welcome!');
+  /* --- Passwords match setup --- */
+  if(passwordEl1.value===passwordEl2.value){
+    /* --- If passwords match --- */
+    passwordsMatch = true;
+    message.style.color = "green";
+    messageContainer.style.borderColor = "green";
   } else {
-    alert('Incorrect username or password');
+    /* --- If password not match --- */
+    passwordsMatch = false;
+    message.textContent = "Make sure passwords match.";
+    message.style.color = "red"
+    messageContainer.style.borderColor = "red";
+    passwordEl1.style.borderColor = "red";
+    passwordEl2.style.borderColor = "red";
+    return;
+  }
+  /* --- If form is valid and password match --- */
+  if(isValid && passwordsMatch){
+    message.textContent = "Registered Successfully!";
+    message.style.color = "green";
+    messageContainer.style.borderColor = "green";
+    passwordEl1.style.borderColor = "green";
+    passwordEl2.style.borderColor = "green";
   }
 }
-confirm(getUser, getPass);
+function storeData(){
+  const user = {
+    name : form.name.value,
+    phone : form.phone.value,
+    mail : form.mail.value,
+    password : form.password2.value
+  }
+  console.log(user);
+}
+
+function processFormData(event) {
+  event.preventDefault();
+  /* --- Validate Form --- */
+  validateForm();
+  /* --- Storing the Data ---*/
+  storeData();
+
+}
+
+form.addEventListener('submit', processFormData);
