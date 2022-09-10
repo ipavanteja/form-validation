3f45b0ff55df333fa2af1c716214c0a26872cafc
pavan432 <pavankunchala0@gmail.com>
2022-09-10 23:54:36 +0530

    completed

--- script.js
+++ script.js
@@ -6,54 +6,83 @@ const message = document.getElementById('message');
 
 let isValid = false;
 let passwordsMatch = false;
+let checkRegister = true;
+let array = [];
 
-function validateForm(){
+function validateForm() {
+  checkRegister = true;
+
   /* --- HTML constraint API to check form validity --- */
   isValid = form.checkValidity();
   /* --- If form is not valid --- */
-  if(!isValid){
+  if (!isValid) {
     /* --- Style main message error --- */
-    message.textContent = "Please fill out all fields.";
-    message.style.color = "red";
-    messageContainer.style.borderColor = "red";
+    message.textContent = 'Please fill out all fields.';
+    message.style.color = 'red';
+    messageContainer.style.borderColor = 'red';
     return;
   }
+
   /* --- Passwords match setup --- */
-  if(passwordEl1.value===passwordEl2.value){
+  if (passwordEl1.value === passwordEl2.value) {
     /* --- If passwords match --- */
     passwordsMatch = true;
-    message.style.color = "green";
-    messageContainer.style.borderColor = "green";
+    message.style.color = 'green';
+    messageContainer.style.borderColor = 'green';
   } else {
     /* --- If password not match --- */
     passwordsMatch = false;
-    message.textContent = "Make sure passwords match.";
-    message.style.color = "red"
-    messageContainer.style.borderColor = "red";
-    passwordEl1.style.borderColor = "red";
-    passwordEl2.style.borderColor = "red";
+    message.textContent = 'Make sure passwords match.';
+    message.style.color = 'red';
+    messageContainer.style.borderColor = 'red';
+    passwordEl1.style.borderColor = 'red';
+    passwordEl2.style.borderColor = 'red';
     return;
   }
-  /* --- If form is valid and password match --- */
-  if(isValid && passwordsMatch){
-    message.textContent = "Registered Successfully!";
-    message.style.color = "green";
-    messageContainer.style.borderColor = "green";
-    passwordEl1.style.borderColor = "green";
-    passwordEl2.style.borderColor = "green";
-  }
-}
-function storeData(){
-  const user = {
-    name : form.name.value,
-    phone : form.phone.value,
-    mail : form.mail.value,
-    password : form.password2.value
-  }
-  if(isValid && passwordsMatch){
-    console.log(user);
+
+  /* --- Already Registered setup ---*/
+  array.push(form.mail.value);
+  if (array.length > 1) {
+    for (let i = 0; i < array.length - 1; i++) {
+      if (array[i] === form.mail.value) {
+        checkRegister = false;
+        if (array.includes(form.mail.value) && checkRegister === false) {
+          array.pop(array.length - 1);
+        }
+        if (checkRegister === false) {
+          break;
+        }
+      }
+    }
   }
 
+  /* --- If alreaady registered ---*/
+  if (!checkRegister) {
+    message.textContent = 'Already Registered.';
+    message.style.color = 'red';
+    messageContainer.style.borderColor = 'red';
+    return;
+  }
+
+  /* --- If form is valid, password match and not registered --- */
+  if (isValid && passwordsMatch && checkRegister) {
+    message.textContent = 'Registered Successfully!';
+    message.style.color = 'green';
+    messageContainer.style.borderColor = 'green';
+    passwordEl1.style.borderColor = 'green';
+    passwordEl2.style.borderColor = 'green';
+  }
+}
+function storeData() {
+  const user = {
+    name: form.name.value,
+    phone: form.phone.value,
+    mail: form.mail.value,
+    password: form.password2.value
+  };
+  if (isValid && passwordsMatch && checkRegister) {
+    console.log(user);
+  }
 }
 
 function processFormData(event) {
@@ -62,7 +91,6 @@ function processFormData(event) {
   validateForm();
   /* --- Storing the Data ---*/
   storeData();
-
 }
 
 form.addEventListener('submit', processFormData);
