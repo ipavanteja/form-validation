7fab605a02dfbbc5bf687f29a9b32f8abc382b26
pavan432 <pavankunchala0@gmail.com>
2023-05-13 20:12:12 +0530

    modification in script.js

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-const form = document.getElementById('form');
-const passwordEl1 = document.getElementById('password1');
-const passwordEl2 = document.getElementById('password2');
-const messageContainer = document.querySelector('.message-container');
-const message = document.getElementById('message');
-const star = document.getElementById('star');
-const reset = document.getElementById('reset');
+const form = document.getElementById("form");
+const passwordEl1 = document.getElementById("password1");
+const passwordEl2 = document.getElementById("password2");
+const messageContainer = document.querySelector(".message-container");
+const message = document.getElementById("message");
+const star = document.getElementById("star");
+const reset = document.getElementById("reset");
 
 let isValid = false;
 let passwordsMatch = false;
@@ -19,9 +19,9 @@ function validateForm() {
   /* --- If form is not valid --- */
   if (!isValid) {
     /* --- Style main message error --- */
-    message.textContent = 'Please fill out all fields.';
-    message.style.color = 'red';
-    messageContainer.style.borderColor = 'red';
+    message.textContent = "Please fill out all fields.";
+    message.style.color = "red";
+    messageContainer.style.borderColor = "red";
     return 1;
   }
 
@@ -29,16 +29,16 @@ function validateForm() {
   if (passwordEl1.value === passwordEl2.value) {
     /* --- If passwords match --- */
     passwordsMatch = true;
-    message.style.color = 'green';
-    messageContainer.style.borderColor = 'green';
+    message.style.color = "green";
+    messageContainer.style.borderColor = "green";
   } else {
     /* --- If password not match --- */
     passwordsMatch = false;
-    message.textContent = 'Make sure passwords match.';
-    message.style.color = 'red';
-    messageContainer.style.borderColor = 'red';
-    passwordEl1.style.borderColor = 'red';
-    passwordEl2.style.borderColor = 'red';
+    message.textContent = "Make sure passwords match.";
+    message.style.color = "red";
+    messageContainer.style.borderColor = "red";
+    passwordEl1.style.borderColor = "red";
+    passwordEl2.style.borderColor = "red";
     return 1;
   }
 
@@ -47,36 +47,36 @@ function validateForm() {
     checkRegister = false;
   }
 
-  /* --- If alreaady registered ---*/
+  /* --- If already registered ---*/
   if (!checkRegister) {
-    message.textContent = 'Already Registered.';
-    message.style.color = 'red';
-    messageContainer.style.borderColor = 'red';
+    message.textContent = "Already Registered.";
+    message.style.color = "red";
+    messageContainer.style.borderColor = "red";
     return 1;
   }
 
   /* --- If form is valid, password match and not registered --- */
   if (isValid && passwordsMatch && checkRegister) {
-    message.textContent = 'Registered Successfully!';
-    message.style.color = 'green';
-    messageContainer.style.borderColor = 'green';
-    passwordEl1.style.borderColor = 'green';
-    passwordEl2.style.borderColor = 'green';
+    message.textContent = "Registered Successfully!";
+    message.style.color = "green";
+    messageContainer.style.borderColor = "green";
+    passwordEl1.style.borderColor = "green";
+    passwordEl2.style.borderColor = "green";
   }
 }
 
 /* --- Show Password --- */
-const togglePassword = document.querySelector('#togglePassword');
-const password = document.querySelector('#password');
+const togglePassword = document.querySelector("#togglePassword");
+const password = document.querySelector("#password");
 
-togglePassword.addEventListener('click', function () {
+togglePassword.addEventListener("click", function () {
   // toggle the type attribute
   const type =
-    passwordEl1.getAttribute('type') === 'password' ? 'text' : 'password';
-  passwordEl1.setAttribute('type', type);
+    passwordEl1.getAttribute("type") === "password" ? "text" : "password";
+  passwordEl1.setAttribute("type", type);
 
   // toggle the icon
-  this.classList.toggle('bi-eye');
+  this.classList.toggle("bi-eye");
 });
 
 /* --- Storing the Data ---*/
@@ -86,7 +86,7 @@ function storeData() {
     name: form.name.value,
     phone: form.phone.value,
     mail: form.mail.value,
-    password: form.password2.value
+    password: form.password2.value,
   };
   if (isValid && passwordsMatch && checkRegister) {
     console.log(user);
@@ -107,12 +107,12 @@ function processFormData(event) {
 function resetForm() {
   form.reset();
   message.textContent = "Don't Hesitate !";
-  message.style.color = 'black';
-  messageContainer.style.borderColor = 'black';
-  passwordEl1.style.borderColor = 'red';
-  passwordEl2.style.borderColor = 'red';
+  message.style.color = "black";
+  messageContainer.style.borderColor = "black";
+  passwordEl1.style.borderColor = "red";
+  passwordEl2.style.borderColor = "red";
 }
 
 /* --- Calling All Functions --- */
-form.addEventListener('submit', processFormData);
-reset.addEventListener('click', resetForm);
+form.addEventListener("submit", processFormData);
+reset.addEventListener("click", resetForm);
